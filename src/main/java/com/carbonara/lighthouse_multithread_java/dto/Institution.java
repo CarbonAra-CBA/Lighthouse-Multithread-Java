f03d7c6c96af82328893a0d650a036f66d29645b
M1nKyu <01.minkyu@gmail.com>
2025-03-22 23:09:05 +0900

    feat: Add InstitutionDto class
    - Institution (기관 정보) 클래스 생성

---
+++ src/main/java/com/carbonara/lighthouse_multithread_java/dto/Institution.java
@@ -0,0 +1,19 @@
+package com.carbonara.lighthouse_multithread_java.dto;
+
+import lombok.*;
+import org.bson.Document;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Institution {
+    private String institutionType;
+    private String institutionCategory;
+    private String institutionSubcategory;
+    private String siteName;
+    private String siteType;
+    private String siteLink;
+
+
